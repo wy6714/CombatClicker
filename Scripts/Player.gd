72e942a1801c4a07b3912b9c249cfa00c86e3d2d
DylWitDaButter <83358505+DylTho1@users.noreply.github.com>
2024-10-25 14:16:10 -0400

    Exp Bar/Levelling Up
    Added an exp bar and levelling up system

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,6 +9,11 @@ extends Node2D
 @export var maxScore: int = 0 # Our TOTAL score accumulated throughout the entirety of playtime. (NOT currency)
 @export var totalClicks: int = 0 # Our total clicks we have done ever. Seems good to track this
 
+@export var level: int = 1
+@export var currentExp: int = 0
+@export var expToNextLevel: int = 100
+@onready var expBarSystem = get_node("/root/Main/ExpBarSystem") # Get a reference to the levelSystem
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,3 +35,17 @@ func determineDamage():
 		crit = true
 		damage = damage * critDamage
 
+func gainExp(exp):
+	currentExp += exp;
+	levelUp()
+	expBarSystem.expBar.value = currentExp
+
+func levelUp():
+	if(currentExp >= expToNextLevel):
+		print("levelled up!")
+		level += 1
+		expBarSystem.levelText.text = "Lv: " + str(level)
+		var expOverflow = currentExp - expToNextLevel
+		currentExp = 0
+		currentExp += expOverflow
+		print("POST LEVEL EXP: ", currentExp)
