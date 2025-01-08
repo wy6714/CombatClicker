4425704da41a92bc9f5b5e6b5975dbb53ff5418f
DylWitDaButter <83358505+DylBDevvin@users.noreply.github.com>
2025-01-08 15:03:16 -0500

    Project Reformatting
    Handles inputs in player script instead of enemy script now

--- Scripts/chickadee.gd
+++ Scripts/chickadee.gd
@@ -23,104 +23,30 @@ func _ready():
 	position = get_viewport().get_size() / 2  # Set position to the center
 	health = health * (1 + 0.2 * (player.level - 1))
 	healthBar.init_health(health)
+	player.currentEnemy = self
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if(charging):
-		chargeLevel += (maxCharge / chargeDuration) * _delta
-		chargeMeter.value = chargeLevel
-		if(chargeLevel >= 100):
-			print("RESET")
-			chargeLevel = 0
+	pass
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	#SWORD
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed: #On mouse click...
-		player.totalClicks += 1 #Track totalClicks
-		player.determineDamage()
-		takeDamage(player.damage)
-		player.score += player.damage #Gain points based on how many points you get each click
-		player.maxScore += player.damage #Increment the maximum
-		updateScore()
-		defeatEnemy()
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed: #On left mouse click...
+		player.dealDamage()
 		
-	#CLAYMORE (on right click)
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
-		if event.pressed:
-			# The right mouse button is pressed
-			print("Right mouse button is pressed")
-			charging = true
-			activateChargeMeter()
-		else:
-			# The right mouse button is released
-			print("Right mouse button is released: CHARGE LEVEL: ", chargeLevel)
-			_on_right_button_released()
-			charging = false
-			dealClaymoreDamage()
-			chargeLevel = 0
-			chargeMeterInstance.queue_free()
-
-func _on_right_button_released():
-	print("Triggered on right mouse button release")
-
-func updateScore():
-	var scoreText = get_node("/root/Main/Scoreboard/ScoreNumber")
-	scoreText.text = str(player.score) #Update text
-
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed: #On right mouse click...
+		player.startingClaymoreAttack = true
+					
 
 func takeDamage(damage):
 	health -= damage
-	DamageNumber.display_number(damage,get_global_mouse_position(),player.crit) #Display damage number and attack animation upon hit
-	activateAttackAnim()
-	player.crit = false
 	healthBar.health = health
-	#var healthText = get_node("/root/Main/Scoreboard/enemy_health_num")
-	#healthText.text = str(health)
-	#print(health)
+	defeatEnemyCheck()
 	
-func defeatEnemy():
+func defeatEnemyCheck():
 	if(health <= 0):
 		player.gainExp(expToGive)
 		enemyManager.spawnEnemy()
 		queue_free()
-	
-func activateAttackAnim():
-	#Later, we would put the code that determines which attack animation we use here
-	# Ex: swap from sword animation to greatsword animation depending on weapon etc
-	
-	var attackingAnimation = attackAnim.instantiate()
-	attackingAnimation.position = to_local(get_global_mouse_position())
-	attackingAnimation.determineAnimation(animComboCount)
-	# This combo index allows for a series of animations, a right slash, a left slash, and then a downward slash. 
-	#Simply allows for a satisfying animation combo
-	animComboCount += 1
-	if(animComboCount >= 3):
-		animComboCount = 0
-	add_child(attackingAnimation)
-	
-func activateChargeMeter():
-	chargeMeterInstance = anim_claymore_meter.instantiate()
-	add_child(chargeMeterInstance)
-	chargeMeter = chargeMeterInstance.get_node("ChargeFill")
-	chargeMeter.value = 0
-	chargeMeter.position = to_local(get_global_mouse_position())
-
-func dealClaymoreDamage():
-	player.totalClicks += 1 #Track totalClicks
-	if(chargeLevel >= 90):
-		player.determineDamage()
-		takeDamage(player.damage * 5)
-		player.score += player.damage * 5 #Gain points based on how many points you get each click
-		player.maxScore += player.damage * 5 #Increment the maximum
-		updateScore()
-		defeatEnemy()
-	else:
-		player.determineDamage()
-		takeDamage(player.damage)
-		player.score += player.damage #Gain points based on how many points you get each click
-		player.maxScore += player.damage #Increment the maximum
-		updateScore()
-		defeatEnemy()
-	
 	
