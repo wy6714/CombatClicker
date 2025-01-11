1041abbba46c19e53774e674b8a3ed464465431a
DylWitDaButter <83358505+DylBDevvin@users.noreply.github.com>
2025-01-10 21:55:22 -0500

    Added Weapon Buttons UI

--- Scripts/add_strength_button.gd
+++ Scripts/add_strength_button.gd
@@ -9,7 +9,11 @@ extends Button
 @export var critRateCost = 100;
 
 @onready var strengthButton = $"."
-@onready var critRateButton = $"../Add Crit Rate"
+@onready var critRateButton = get_node("/root/Main/shop/Add Crit Rate")
+
+@onready var swordHolder = get_node("/root/Main/shop/Sword")
+@onready var claymoreHolder =  get_node("/root/Main/shop/Claymore")
+@onready var drillHolder =  get_node("/root/Main/shop/Drill")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -38,3 +42,46 @@ func _on_add_crit_rate_button_up():
 		player.critRate += 0.5
 		critRateText.text = str(player.critRate)
 		critRateButton.text = "+0.5 Crit Rate (%d)" %[critRateCost]
+
+func _on_sword_button_down():
+	toggle_visibility(swordHolder)
+
+func _on_sword_1_button_down():
+	pass # Replace with function body.
+
+func _on_sword_2_button_down():
+	pass # Replace with function body.
+
+func _on_sword_3_button_down():
+	pass # Replace with function body.
+	
+func _on_claymore_button_down():
+	toggle_visibility(claymoreHolder)
+	
+func _on_claymore_1_button_down():
+	pass # Replace with function body.
+	
+func _on_claymore_2_button_down():
+	pass # Replace with function body.
+
+func _on_claymore_3_button_down():
+	pass # Replace with function body.
+
+func _on_drill_button_down():
+	toggle_visibility(drillHolder)
+	
+func _on_drill_1_button_down():
+	pass # Replace with function body.
+
+func _on_drill_2_button_down():
+	pass # Replace with function body.
+
+func _on_drill_3_button_down():
+	pass # Replace with function body.
+
+func toggle_visibility(weapon_node: Node):
+	for child in weapon_node.get_children():
+		if child.is_visible():
+			child.hide()
+		else:
+			child.show()
