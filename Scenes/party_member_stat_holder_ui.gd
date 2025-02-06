9a459897bbcd5fed98d9c1cfc25dd057041b6bcc
DylWitDaButter <83358505+DylBDevvin@users.noreply.github.com>
2025-02-06 13:22:20 -0500

    UI tweaks
    Added tweaks and some functionality to UI

--- Scenes/party_member_stat_holder_ui.gd
+++ Scenes/party_member_stat_holder_ui.gd
@@ -6,6 +6,12 @@ extends Control
 @onready var ultRegenVal = $UltRegenPoints
 @onready var cooldownVal = $CooldownPoints
 
+@onready var upgradePointText = $UpgradePoints
+@onready var upgradePointCostText = $BuyUpgrade/UpgradePointsCost
+@export var upgradePoints: int = 0
+@export var upgradePointCost: int = 1000
+@export var upgradeCostMultiplier: float = 1.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,4 +27,10 @@ func updateAllValues(strength, critRate, critDamage, ultRegen, cooldown):
 	critDamageVal.text = str(critDamage)
 	ultRegenVal.text = str(ultRegen)
 	cooldownVal.text = str(cooldown)
-		
+	
+func _on_buy_upgrade_button_down():
+	upgradePoints += 1
+	upgradePointText.text = "Upgrade Points " + str(upgradePoints)
+	upgradeCostMultiplier += 0.1
+	upgradePointCost *= upgradeCostMultiplier
+	upgradePointCostText.text = str(upgradePointCost) + " points"
